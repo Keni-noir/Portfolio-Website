1a6ef2549adac019d5d38eafad2a26459f61a1ab
Keni-noir <keninoir03@example.com>
2026-03-09 13:06:38 +0000

    new project update

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,10 +4,20 @@ import ResultSummary from "/Project/Result-Summary.png"
 import Gracefootwears from "/Project/Gracefootwears.png" 
 import ApiCountries from "/Project/Api-Countries.png"
 import MuizAmir from "/Project/MuizAmir.png"
+import Bookstore from "/Project/bookstore.png"
 
 const Projects = [
-    {
+   {
         id: 1,
+     title: "Bookstore Website ",
+     description: "A bookstore web app. User can create books or add book which would be shown on their dashboard. The discovery page consusmes api from open library to show books and user can add book from discovery page to their dashboard. User can also update or delete book from their dashboard. The backend is built with nodejs, express and postgres, while the frontend is built with react and tailwindcss.",
+     image: Bookstore,
+     tags: ["React", "TailwindCSS", "Nodejs", "Express", "Postgres ", "Supabase"],
+     demoURL: "bookstore-frontend-amber.vercel.app",
+     GithubRepo:"https://github.com/Keni-noir/Bookstore-Frontend",
+     },
+    {
+        id: 2,
      title: "MuizAmir E-commerce website ",
      description: "An E-comerce website using Next.js and tailwind for a client, hosted on vercel. Website contains product of client and help buys place order that are sent to companies whatsapp due to client's request.  ",
      image: MuizAmir ,
@@ -16,7 +26,7 @@ const Projects = [
      GithubRepo:"https://github.com/Keni-noir/MuizAmir",
      },
      {
-        id: 2,
+        id: 3,
      title: "E-comerce Website for a Footwear store",
      description: "AN  E-comerce website built with Nextjs, tailwindCss, implementing stripe for payment and handling and retrival of product data via API",
      image: Gracefootwears ,
@@ -25,7 +35,7 @@ const Projects = [
      GithubRepo:"https://github.com/Keni-noir/E-commerce-website-",
      },
     {
-        id: 3,
+        id: 4,
      title: "Shopping-Cart Landing Page ",
      description: "A beautiful Shopping Landing page app using React and Tailwind, aimed at learning state management with useState and props drilling. Radux toolkit was used for global state management.",
      image: ShoppingCart ,
@@ -35,7 +45,7 @@ const Projects = [
      },
 
      {
-        id: 4,
+        id: 5,
      title: "Mortgage Calculator Landing Page ",
      description: "Mortgage Calculator Landing page app using React and Tailwind, aim at handling forms in react and basic javascript calculations.",
      image: MortgageCalculator ,
@@ -45,7 +55,7 @@ const Projects = [
      },
 
      {
-        id: 5,
+        id: 6,
      title: "Result Summary Landing Page ",
      description: "A Responsive Result page app using React and Tailwind, aim at learning more about flexbox, grid layouts and site responsiveness in TailwindCSS.",
      image: ResultSummary ,
@@ -55,7 +65,7 @@ const Projects = [
      },
 
      {
-        id: 6,
+        id: 7,
      title: "API Countries ",
      description: "A Responsive web app using React and Tailwind, learning how to fetch Restful API and make use if it in the project ",
      image: ApiCountries ,
